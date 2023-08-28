e72a7ab89e36eb354627710021ac38850e4fe1ec
Soilenyis <soilenyis@gmail.com>
2023-08-28 16:38:53 -0300

    implentando dados com scanner

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite seu nome: "); 
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite sua Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o numero da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o saldo da sua conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque " ); 
+
+        scanner.close();
     }
 }
